35d4e27dc0f5c6eda1a85f9840734a47406aa264
Wenxuan Su <35317989+stony-su@users.noreply.github.com>
2024-08-22 11:10:27 -0400

    bug fix

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,6 +1,6 @@
 function redirectToPage() {
     // Get the value from the input field
-    var city = document.getElementById('demo-city').value;
+    var city = document.getElementById('city').value;
 
     // Construct the URL based on the input value
     var url = city + '.html';
